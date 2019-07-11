c81d38f6b8fe1e620c82e5ae26102df7d06f51c9
Andrew Santos <asantos@alumni.nd.edu>
2019-07-11 23:32:31 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -51,7 +51,7 @@ def apply_coupons(cart, coupons)
     while coupons[counter] do
       if coupons[counter][:item] == key
         new_hash[key][:count] -= coupons[counter][:num]
-        coupon_list["#{key} W/COUPON"]= {:price => coupons[counter][:cost]/coupons[counter][:num], :clearance => new_hash[key][:clearance]}#, :count => coupons[counter][:num]}
+        coupon_list["#{key} W/COUPON"]= {:price => coupons[counter][:cost]/coupons[counter][:num], :clearance => new_hash[key][:clearance], :count => coupons[counter][:num]}
         return coupon_list
       end 
     end 
