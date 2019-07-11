cd5db1302522d483cd1d2d10cb1d2f44a19bc54c
Andrew Santos <asantos@alumni.nd.edu>
2019-07-11 23:17:34 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -45,7 +45,12 @@ puts new_cart
 
 def apply_coupons(cart, coupons)
   new_hash = cart
-  new_hash.each {|key, value|}
+  new_hash.each {|key, value|
+    counter = 0
+    while coupons[counter] do
+      if coupons[counter][:item] == key
+        return key
+  }
 end
 
 #puts apply_coupons(new_cart, coupon)
