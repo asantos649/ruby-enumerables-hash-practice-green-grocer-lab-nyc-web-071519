ee5737f55ba5994e36799fea04ac6dffb5ff4381
Andrew Santos <asantos@alumni.nd.edu>
2019-07-12 00:51:30 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -12,9 +12,9 @@ first_list =     [
     ]
 
 coupon = [
-      {:item => "AAVOCADO", :num => 2, :cost => 5.00},
-      {:item => "ABEER", :num => 2, :cost => 20.00},
-      {:item => "ACHEESE", :num => 3, :cost => 15.00}
+      {:item => "AVOCADO", :num => 2, :cost => 5.00},
+      {:item => "BEER", :num => 2, :cost => 20.00},
+      {:item => "CHEESE", :num => 3, :cost => 15.00}
     ]
 
 def consolidate_cart(cart)
