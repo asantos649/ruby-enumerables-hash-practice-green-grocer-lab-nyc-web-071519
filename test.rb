20d9a594d171ef77fad55b5071ddbd74056e4d8f
Andrew Santos <asantos@alumni.nd.edu>
2019-07-12 01:05:03 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@ first_list =     [
       {"AVOCADO" => {:price => 3.00, :clearance => false}},
       {"KALE" => {:price => 3.00, :clearance => false}},
       {"BLACK_BEANS" => {:price => 2.00, :clearance => false}},
-#      {"ALMONDS" => {:price => 10.00, :clearance => false}},
+      {"ALMONDS" => {:price => 10.00, :clearance => false}},
 #      {"TEMPEH" => {:price => 3.00, :clearance => false}},
 #      {"CHEESE" => {:price => 6.00, :clearance => false}},
 #      {"BEER" => {:price => 13.00, :clearance => false}},
