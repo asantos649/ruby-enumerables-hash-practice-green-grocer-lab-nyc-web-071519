4229a891179f579a9b02b618d30e21f6fa91d005
Andrew Santos <asantos@alumni.nd.edu>
2019-07-12 00:24:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -52,7 +52,9 @@ def apply_clearance(cart)
   cart.each{|key, value|
     if cart[key]["clearance"]
       cart[key][:price] = 0.8*cart[key][:price]
+    end
   }
+  return cart
 end
 
 def checkout(cart, coupons)
